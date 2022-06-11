0d43a8304b5b4d6cf2bdd3750908549a839bb30c
ConsensusAI <malik.iqbal@hotmail.ca>
2022-06-10 20:06:32 -0400

    Implement conditional rendering of show or empty component in appointment

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,7 +1,16 @@
 import React from "react";
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
+import { Fragment } from "react";
 
 import "./styles.scss";
 
 export default function Appointment(props) {
-  return <article className="appointment"></article>;
+  return (
+    <article className="appointment">
+      <Header time={props.time} />
+      {props.interview ? <Show /> : <Empty />}
+    </article>
+  );
 }
