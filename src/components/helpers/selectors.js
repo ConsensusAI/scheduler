222ac88d4758af9b47326bef2608b057d7577df4
ConsensusAI <malik.iqbal@hotmail.ca>
2022-06-12 17:40:53 -0400

    Combine states into one object and fix daylistitem variable reference

---
+++ src/components/helpers/selectors.js
@@ -0,0 +1,22 @@
+export function getAppointmentsForDay(state, day) {
+  let res = [];
+
+  if (!state.appointments) {
+    return res;
+  }
+
+  let apptArr = [];
+  for (let el in state.days) {
+    if (state.days[el].name == day) {
+      apptArr = state.days[el].appointments;
+    }
+  }
+
+  for (let id of apptArr) {
+    if (state.appointments[id]) {
+      res.push(state.appointments[id]);
+    }
+  }
+
+  return res;
+}
